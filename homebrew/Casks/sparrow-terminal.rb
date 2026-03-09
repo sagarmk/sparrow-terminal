b775d583c93552e7a21856d7cf11e733d9c8cd56
Sagar M <mankarisagar@gmail.com>
2026-03-09 00:09:08 -0400

    Update download links and version badge to v1.6.2-beta

--- homebrew/Casks/sparrow-terminal.rb
+++ homebrew/Casks/sparrow-terminal.rb
@@ -1,6 +1,6 @@
 cask "sparrow-terminal" do
-  version "1.6.1-beta"
-  sha256 "e8062eae6f3bb748461bbc3a63cf3b96c370ea65925a3b25e9e4dee8482f3222"
+  version "1.6.2-beta"
+  sha256 "44f86eb232e30958b571fb23d0884cffdcbf838c107e6728a51f1499b3db9881"
 
   url "https://github.com/sagarmk/sparrow-terminal/releases/download/v#{version}/sparrow-#{version}-arm64.dmg"
   name "Sparrow"
